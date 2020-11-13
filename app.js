649b1b7f29e20f64115be85d29c065a409ea85bf
Ifeanyi Fredrick Okwuobi <fredrickokwuobi@gmail.com>
2020-11-13 11:37:05 +0100

    updated nav bar js and styles

--- app.js
+++ app.js
@@ -1,90 +1,36 @@
+const navigation = document.querySelector(".navigation");
+const burger = document.querySelector(".burger");
+const navLinks = [...document.querySelectorAll(".nav-link")];
 
-// console.log
-const responsiveNav=()=>{
-  let burger = document.querySelector(".nav-burger");
-  let nav = document.querySelector(".navigation");
-  let [...navLinks] = document.querySelectorAll(".navigation li");
-  // console.log(navLinks);
-  burger.addEventListener("click", () => {
-    navLinks.forEach((link,index)=>{
-      if(link.style.animation){
-        link.style.animation = "";
-      }else{
-        link.style.animation = `linkfade 0.5s ease forwards ${index/9 + 0.2}s`
-        // link.style.animation = `transition all 1s ease`;
-      }
-    })
-    nav.classList.toggle('nav-active');
-    burger.classList.toggle("toggle");
-
-  });
-}
-
-const fixCrazyLinks =()=>{
-  let [...headers] = document.querySelectorAll(".header");
-  let nav = document.querySelector("#navbar");
-  let [...allLinks] = document.querySelectorAll(".navigation a");
-  allLinks.find((link,index)=>{
-    link.addEventListener("click",()=>{
-      headers.find((header)=>{
-        if(link.innerHTML == header.innerText){
-          // header.classList.add("headSpace");
-          //this makes the current header have more padding space so it can be seen by the user
-          // nav.classList.add("hidenav");
-          // setTimeout(()=>{
-          //   header.classList.remove("headSpace");
-          // },10000);
-        }else{
-          // header.classList.remove("headSpace");
-          // nav.classList.remove("hidenav")
-        }
-      })
-     })
-    })
-}
-// this js helps to fixed the navbar as we scroll thanks w3School for this
-const stickyNav=()=>{
-  // get the navbar
-  let navbar = document.querySelector("#navbar");
-  window.addEventListener('scroll',() => {
-    addRemoveSticky();
-  });
-
-  let navLink = document.querySelectorAll(".navigation a");
-
-  // get the offset position of the navbar
-  let sticky = navbar.offsetTop;
-  // add the sticky class to the navbar when we reach its scroll position & remove the sticky class when we leave the scroll position
-  const addRemoveSticky =()=>{
-    if(window.pageYOffset >= sticky){
-      navbar.classList.add("sticky");
-    } else {
-      navbar.classList.remove("sticky");
+// this function adds the animation css class to whatever link was passed to it
+function animateLinks(links){
+  links.forEach((link,index)=>{
+    if(link.style.animation){
+      link.style.animation = "";
+    }else{
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 20 + 0.3}s`;
     }
-  }
+  })
 }
-// const changeLinkStyle = ()=>{
-// let [...links] = document.querySelectorAll(".nav-link");
-// let [...headers] = document.querySelectorAll(".header");
-//     links.find(link=>{
-//       link.addEventListener("click",()=>{
-//         headers.find(header=>{
-//           if(link.innerHTML == header.innerText){
-//             link.classList.add("active-link");
-//           }else if(link.style == "active-link") {
-//             link.classList.remove("active-link");
-//           }
-//         })
-//       })
-//     })
-
-// }
-
-
-const app =()=>{
-  responsiveNav();
-  stickyNav();
-  // fixCrazyLinks();
-  // changeLinkStyle();
+// this othe function lets the user close the nav bar when the user clicks on any nav link item
+function toggleNavFromLink(navLinks){
+  navLinks.forEach((link)=>{
+    link.addEventListener("click",()=>{
+      navigation.classList.toggle("nav-active");
+      // this line of code animates the links as they enter onto the display
+      animateLinks(navLinks);
+      burger.classList.toggle("toggle");
+    });
+  });
 }
-app();
+// this function does the main work of calling the other functions
+const navSlide = ()=>{
+  burger.addEventListener("click",()=>{
+    navigation.classList.toggle("nav-active");
+    animateLinks(navLinks);
+    burger.classList.toggle("toggle");
+  });
+  // this toggles the responsive nav bar from the nav link
+  toggleNavFromLink(navLinks);
+}
+navSlide();
